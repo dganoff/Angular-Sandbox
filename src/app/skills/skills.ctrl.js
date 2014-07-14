f16b637357f063ab13514044b80ab555d9695ea7
Demetri Ganoff <dganoff@gmail.com>
2014-07-13 23:32:03 -0400

    moved to their own folder

---
+++ src/app/skills/skills.ctrl.js
@@ -0,0 +1,37 @@
+angular.module('sandbox').controller('skillsCtrl', ['$scope', '$state', 'skillsService', function($scope, $state, skillsService) {
+	$scope.skills = [
+		 {
+		 	name: "HTML 5",
+		 	type: "testing"
+		 },
+		 {
+		 	name: "SASS",
+		 	type: "testing"
+		 },
+		 {
+		 	name: "CSS 3 Animations",
+		 	type: "testing"
+		 },
+		 {
+		 	name: "JavaScript",
+		 	type: "testing"
+		 },
+		 {
+		 	name: "Angular JS",
+		 	type: "testing"
+		 },
+		 {
+		 	name: "Mobile Apps",
+		 	type: "testing"
+		 },
+	];
+
+	$scope.selectSkill = function(skill) {
+		skillsService.name = skill;
+
+		// Scroll to the top of the page so you can see the detail page slide in:
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
+		
+		$state.go('skillDetails');
+	};
+}]);
